07557594fcf29c19f9bbd2a115878aea13f595fe
cartesdec <cartesius1596@gmail.com>
2026-07-15 08:52:21 +0200

    Add QML support for empty window - update main.cpp

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -1,11 +1,15 @@
-#include <QCoreApplication>
-#include <QDebug>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
-    
-    qDebug() << "hello";
-    
-    return 0;
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
