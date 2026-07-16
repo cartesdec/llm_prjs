71aedae0afd94b4cb1d2a1123f811245fac5fa4a
cartesdec <cartesius1596@gmail.com>
2026-07-16 22:22:55 +0200

    Add role property to Person class

--- project_1/person.h
+++ project_1/person.h
@@ -9,6 +9,7 @@ class Person : public QObject
     Q_OBJECT
     Q_PROPERTY(QString firstname READ firstname WRITE setFirstname NOTIFY firstnameChanged)
     Q_PROPERTY(QString surname READ surname WRITE setSurname NOTIFY surnameChanged)
+    Q_PROPERTY(QString role READ role WRITE setRole NOTIFY roleChanged)
 
 public:
     explicit Person(QObject *parent = nullptr);
@@ -19,13 +20,18 @@ public:
     QString surname() const;
     void setSurname(const QString &surname);
 
+    QString role() const;
+    void setRole(const QString &role);
+
 signals:
     void firstnameChanged(const QString &firstname);
     void surnameChanged(const QString &surname);
+    void roleChanged(const QString &role);
 
 private:
     QString _firstname;
     QString _surname;
+    QString _role;
 };
 
 #endif // PERSON_H
