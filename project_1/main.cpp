cd6599f113b0375a0e8656071c15882e6a9678f3
cartesdec <cartesius1596@gmail.com>
2026-07-16 22:19:27 +0200

    Create Person instance and export to QML context

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -1,11 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "person.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    
+    // Create Person instance
+    Person customer;
+    
+    // Export customer object to QML context
+    engine.rootContext()->setContextProperty("customer", &customer);
+    
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     if (engine.rootObjects().isEmpty())
