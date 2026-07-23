a7e716f2ed3ab4ff47085243b0bd819eb60ec6c0
cartesdec <cartesius1596@gmail.com>
2026-07-23 21:46:02 +0200

    Add address property implementations to Person class

--- project_1/person.cpp
+++ project_1/person.cpp
@@ -1,7 +1,7 @@
 #include "person.h"
 
 Person::Person(QObject *parent)
-    : QObject(parent), _firstname(""), _surname(""), _role("")
+    : QObject(parent), _firstname(""), _surname(""), _role(""), _streetAddress(""), _city(""), _postalCode(""), _country("")
 {
 }
 
@@ -41,5 +41,57 @@ void Person::setRole(const QString &role)
     if (_role != role) {
         _role = role;
         emit roleChanged(_role);
+    }
+}
+
+QString Person::streetAddress() const
+{
+    return _streetAddress;
+}
+
+void Person::setStreetAddress(const QString &streetAddress)
+{
+    if (_streetAddress != streetAddress) {
+        _streetAddress = streetAddress;
+        emit streetAddressChanged(_streetAddress);
+    }
+}
+
+QString Person::city() const
+{
+    return _city;
+}
+
+void Person::setCity(const QString &city)
+{
+    if (_city != city) {
+        _city = city;
+        emit cityChanged(_city);
+    }
+}
+
+QString Person::postalCode() const
+{
+    return _postalCode;
+}
+
+void Person::setPostalCode(const QString &postalCode)
+{
+    if (_postalCode != postalCode) {
+        _postalCode = postalCode;
+        emit postalCodeChanged(_postalCode);
+    }
+}
+
+QString Person::country() const
+{
+    return _country;
+}
+
+void Person::setCountry(const QString &country)
+{
+    if (_country != country) {
+        _country = country;
+        emit countryChanged(_country);
     }
 }
